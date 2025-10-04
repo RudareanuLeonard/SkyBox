ceca2d2c1c958534af251d708bd3f874b76cf1f9
RudareanuLeonard <leonardrudareanu@gmail.com>
2025-10-04 20:36:54 +0300

    made some progress on Server but not entirely functional yet

--- Server/Server.cpp
+++ Server/Server.cpp
@@ -2,12 +2,42 @@
 
 Server::Server(){
     std::cout << "This is my Server class constructor\n";
-    server_sockaddr.sa_family = AF_INET;
+    server_sockaddr_in.sin_family = AF_INET;
+    server_sockaddr_in.sin_addr.s_addr = inet_addr("127.0.0.1");
+    server_sockaddr_in.sin_port = PORT;
 
     server_addrinfo.ai_family = AF_INET;
     server_addrinfo.ai_socktype = SOCK_STREAM;
     server_addrinfo.ai_protocol = 0; // 0 = any
     server_addrinfo.ai_addrlen = 4; //length in bytes
-    server_addrinfo.ai_addr = &server_sockaddr;
+    server_addrinfo.ai_addr = (struct sockaddr *)&server_sockaddr_in;
+}
 
+void Server::start_server(){
+    
+    int sockfd = socket(this->server_addrinfo.ai_family, this->server_addrinfo.ai_socktype, this->server_addrinfo.ai_protocol);
+
+    int check_bind = bind(sockfd, (struct sockaddr *)&server_sockaddr_in, 4);
+
+    if(check_bind < 0){
+        std::cout << "Bind failed!\n";
+        std::exit(0);
+    }
+
+    int check_listen = listen(sockfd, BACKLOG);
+
+    if(check_listen < 0){
+        std::cout << "Listen failed!\n";
+        std::exit(0);
+    }
+
+    int client_sock = accept(sockfd, nullptr, nullptr);
+}
+
+int main(void){
+    std::cout << "Hello, there is Server.cpp file. I am gonna start a server...\n\n";
+
+    Server server;
+
+    server.start_server();
 }
--- Server/Server.h
+++ Server/Server.h
@@ -1,12 +1,14 @@
 #include <iostream>
 #include <cstring>
-
 #include <sys/socket.h>
 #include <sys/types.h>
 #include <netinet/in.h>
+#include <arpa/inet.h>
 #include <netdb.h>
 
 const std::string LOCALHOST = "127.0.0.1";
+const int PORT = 8080;
+const int BACKLOG = 5;
 
 //addrinfo
 //sockaddr_in <- for IPv4
@@ -14,15 +16,11 @@ const std::string LOCALHOST = "127.0.0.1";
 class Server{
     private:
     addrinfo server_addrinfo = addrinfo(); // we declare it here because addrinfo is a PODT
-    sockaddr server_sockaddr; // we declare it sockaddr and not sockaddr_in (specific for ipv4) in order to pass it to server_addrinfo (addrinfo has a field of type sockaddr)
+    sockaddr_in server_sockaddr_in; // we declare it sockaddr and not sockaddr_in (specific for ipv4) in order to pass it to server_addrinfo (addrinfo has a field of type sockaddr)
 
 
     public:
     Server();
+    void start_server();
 
-    void start_server(){
-        
-
-    }
-
-}
+};
